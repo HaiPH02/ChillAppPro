c4ed61a015153367eff39234068e493622f8b22e
Hai Pham <haipham0208@gmail.com>
2025-08-30 10:22:52 +0700

    Initial Commit

---
+++ ChillAppPro/ChillAppProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChillAppProApp.swift
+//  ChillAppPro
+//
+//  Created by HaiPH on 30/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ChillAppProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
